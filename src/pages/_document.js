a3ce1f452a3b373b2ad6444f8ce7cb6ed7201433
Eduardo <kratosedu18@gmail.com>
2023-05-01 00:29:46 -0600

    de nuevo el async

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <title>Portafolio</title>
         
-      <script src="https://unpkg.com/scrollreveal"/>
+      <script async src="https://unpkg.com/scrollreveal"/>
   
       <Head />
       <body>
