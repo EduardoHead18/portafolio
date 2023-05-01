4cb4fd03c21fff173fc79c7251bff79d369dce00
Eduardo <kratosedu18@gmail.com>
2023-05-01 00:37:23 -0600

    ok

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <title>Portafolio</title>
         
-      <script async src="https://unpkg.com/scrollreveal"/>
+
   
       <Head />
       <body>
